4bb19ccbbd72620f4bf2b886fc8cad446c90551e
Romyojit Paul <rabinpaul110@gmail.com>
2025-04-17 21:12:23 +0530

    modified:   Symbolic_Math/Two_var_eq_solver.py

--- Symbolic_Math/Two_var_eq_solver.py
+++ Symbolic_Math/Two_var_eq_solver.py
@@ -7,9 +7,9 @@ def eq_solver(eq1, eq2): ### CAN BE IMPORTED ###
     return solved
 
 eq1 = sp.Eq(5*x + 2*y, 12)
-eq2 = sp.Eq(3* + 5*y, 15)
+eq2 = sp.Eq(2*x + 5*y, 9)
 
-print("Equation:", end=' ')
+print("Equation:\n")
 print(eq1.lhs, '=', eq1.rhs)
 print(eq2.lhs, '=', eq2.rhs)
 print("")
