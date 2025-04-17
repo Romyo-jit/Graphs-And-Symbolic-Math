c9e8a4aa178cfd52b377db68e41c1e2c5bf401be
Romyojit Paul <rabinpaul110@gmail.com>
2025-04-17 21:09:52 +0530

    new file:   Symbolic_Math/Two_var_eq_solver.py

---
+++ Symbolic_Math/Two_var_eq_solver.py
@@ -0,0 +1,18 @@
+import sympy as sp
+
+x, y = sp.symbols('x y')
+
+def eq_solver(eq1, eq2): ### CAN BE IMPORTED ###
+    solved = sp.solve([eq1, eq2], (x, y))
+    return solved
+
+eq1 = sp.Eq(5*x + 2*y, 12)
+eq2 = sp.Eq(3* + 5*y, 15)
+
+print("Equation:", end=' ')
+print(eq1.lhs, '=', eq1.rhs)
+print(eq2.lhs, '=', eq2.rhs)
+print("")
+print("Solution:", end='\n\n')
+
+sp.pprint(eq_solver(eq1, eq2), use_unicode=True)
