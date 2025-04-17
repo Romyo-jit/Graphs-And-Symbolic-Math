873ac3060a5e07267d6d4cf267cdf8a436f8f064
Romyojit Paul <rabinpaul110@gmail.com>
2025-04-17 21:03:10 +0530

    new file:   .gitignore 	new file:   Symbolic_Math/One_var_eq_solver.py

---
+++ Symbolic_Math/One_var_eq_solver.py
@@ -0,0 +1,15 @@
+import sympy as sp
+
+x = sp.symbols('x')
+
+def eq_solver(eq): ### CAN BE IMPORTED ###
+    solved = sp.solve(eq)
+    return solved
+
+eq = sp.Eq(x**2 + 9*x + 1, 0)
+
+print("Equation:", end=' ')
+print(eq.lhs, '=', eq.rhs)
+print("Solution:", end='\n\n')
+sp.pprint(eq_solver(eq), use_unicode=True)
+   
